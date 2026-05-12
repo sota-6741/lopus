8cde00d9524b5af5defab12338eae55b89d267a0
sota-6741 <207503862+sota-6741@users.noreply.github.com>
2026-05-12 21:07:53 +0900

    add: ドメイン全体で共有するBranded Typesの定義を追加

---
+++ apps/backend/src/domain/user/user.types.ts
@@ -0,0 +1,15 @@
+// Identifier
+export type UserId = string & { readonly __brand: "UserId" };
+
+// User
+export type UserRole = "admin" | "general";
+
+// Profile
+export type UserName = string & { readonly __brand: "UserName" };
+export type IconUrl = string & { readonly __brand: "IconUrl" };
+
+// University (Reference)
+export type UniversityId = string & { readonly __brand: "UniversityId" };
+
+// Password
+export type HashedString = string & { readonly __brand: "HashedString" };
