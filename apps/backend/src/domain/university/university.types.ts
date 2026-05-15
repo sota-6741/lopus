79eae2c9f43bd9a37101e8f602704b8db5e2a0ca
sota-6741 <207503862+sota-6741@users.noreply.github.com>
2026-05-15 23:47:36 +0900

    refactor: 大学集約に関連する型定義を更新

--- apps/backend/src/domain/university/university.types.ts
+++ apps/backend/src/domain/university/university.types.ts
@@ -1,2 +1,8 @@
-export type UniversityId = string & { readonly　__brand: "UniversityId" };
-export type UniversityDomain = string & { readonly __brand: "UniversityDomain" };
+import { IIdentifier } from "../shared/shared.types";
+
+export type UniversityId = string &
+  IIdentifier & { readonly __brand: "UniversityId" };
+export type UniversityName = string & { readonly __brand: "UniversityName" };
+export type UniversityDomain = string & {
+  readonly __brand: "UniversityDomain";
+};
